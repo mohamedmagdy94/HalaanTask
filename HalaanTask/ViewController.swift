cbc6266ae82a18aeb794b30ea6ab6131fbbf3026
Mohamed El-Taweel <mohamedmagdy.9080@gmail.com>
2022-10-18 12:42:06 +0200

    Initial Commit

---
+++ HalaanTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HalaanTask
+//
+//  Created by Mohamed on 18/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
