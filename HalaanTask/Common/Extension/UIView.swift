06c6723536e5e79483217ee9d8bdd1758cf4fec9
Mohamed El-Taweel <mohamedmagdy.9080@gmail.com>
2022-10-19 22:29:41 +0200

    Tweet counter ui
    Assets + Extension + View

---
+++ HalaanTask/Common/Extension/UIView.swift
@@ -0,0 +1,43 @@
+//
+//  UIView.swift
+//  HalaanTask
+//
+//  Created by Mohamed on 19/10/2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            let color = UIColor.init(cgColor: layer.borderColor!)
+            return color
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+}
---
+++ HalaanTask/feature/TweetCounter/View/TweetCounterViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TweetCounterViewController.swift
+//  HalaanTask
+//
+//  Created by Mohamed on 19/10/2022.
+//
+
+import UIKit
+
+class TweetCounterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
